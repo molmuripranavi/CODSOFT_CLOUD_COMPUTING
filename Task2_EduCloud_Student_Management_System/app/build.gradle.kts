b15d5f185af40b962619d2480380663d97f88a6b
molmuripranavi <molmuripranavi@gmail.com>
2026-07-16 14:30:56 +0530

    login screen

--- Task2_EduCloud_Student_Management_System/app/build.gradle.kts
+++ Task2_EduCloud_Student_Management_System/app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
 
     implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
