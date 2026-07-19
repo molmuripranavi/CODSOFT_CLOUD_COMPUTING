5e41668ed6969630f364c98996fa9f1480e4f6c7
molmuripranavi <molmuripranavi@gmail.com>
2026-07-19 14:48:19 +0530

    updated version

---
+++ Task2_EduCloud_Student_Management_System/app/src/main/java/com/molmuripranavi/educloud/models/LeaveRequest.kt
@@ -0,0 +1,21 @@
+package com.molmuripranavi.educloud.models
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.molmuripranavi.educloud.R
+
+class LeaveRequest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_leave_request)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
